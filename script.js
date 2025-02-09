3b2a7eb2be4eb3155dfc05facdeaf3afb6c173ec
valen3212 <valentinnicolas663@gmail.com>
2025-02-09 19:52:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btnYes").addEventListener("click", function() {
+    window.location.href = "sorpresa.html"; // Redirige a la nueva página
+});
+
+document.getElementById("btnNo").addEventListener("click", function() {
+    alert("Tal vez en otro momento :)");
+});
