b238ce6822da1753bed7eba06bd880380a4295ff
valen3212 <valentinnicolas663@gmail.com>
2025-02-09 19:51:11 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("btnYes").addEventListener("click", function() {
-    const animation = document.getElementById("animation");
-    animation.classList.remove("hidden");
-});
-
-document.getElementById("btnNo").addEventListener("click", function() {
-    alert("Tal vez en otro momento :)");
-});
